577ee7f02c4fae37c629e8ffe5a4e083d8c271c0
Jadev03 <sathiendrathabendra@gmail.com>
2025-03-26 07:26:20 +0530

    setup: tsx support,tailwind css,Mui setup

---
+++ front-end/src/user/Layout/layoutcomponent/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// mui imports
+import{
+
+   Drawer
+   
+   
+
+} from "@mui/material"
+
+const drawerelements=[
+    []
+]
+
+const Sidebar = () => (
+    <Drawer >
+
+    </Drawer>
+)
+  
+
+
+export default Sidebar
