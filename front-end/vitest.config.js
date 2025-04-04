30b8808d7855cc4e36b02eefe33741b249c94a5e
SivaNiroshan <sniroshan1964@gmail.com>
2025-04-04 19:03:13 +0530

    ok

---
+++ front-end/vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+      coverage: {
+        provider: 'v8', // or 'istanbul'
+        reporter: ['text', 'json', 'html', 'lcov', 'xml'],
+        reportsDirectory: './coverage'
+      }
+    }
+  })
